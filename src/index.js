d826083d375866a204dee29ed01ec6608efbc740
George Partida <george.partida@gmail.com>
2017-08-29 12:21:39 -0700

    Removed unnecessary routing imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './css/site.css';
 import App from './components/App';

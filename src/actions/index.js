0e92e460a4ecbd533d0d720d58f0684a0a047e82
George Partida <george.partida@gmail.com>
2017-08-30 14:45:27 -0700

    Added example of setting redux store state before anything is rendered, also demonstrates using localstorage for remembering user is signed in

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,9 +6,11 @@ export function authenticate(isLoggedIn) {
 
     if (isLoggedIn) {
         history.push('/protected');
+        localStorage.setItem('auth_token', 'place_jwt_token_here')
     }
     else {
         history.push('/');
+        localStorage.removeItem('auth_token');
     }
 
     return {
--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ import { createStore, applyMiddleware } from 'redux';
 import './css/site.css';
 import App from './components/App';
 import rootReducer from './reducers/rootReducer';
+import * as ActionTypes from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
+if (localStorage.getItem('auth_token')) {
+    store.dispatch({
+        type: ActionTypes.CHANGE_AUTH,
+        payload: true
+    });
+}
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(rootReducer)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.querySelector('.container')
